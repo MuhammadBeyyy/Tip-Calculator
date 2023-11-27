80da10c3c3cf3e6db521f77681ecd86bc58b1c70
MuhammadBeyyy <146830245+MuhammadBeyyy@users.noreply.github.com>
2023-11-27 00:05:40 -0800

    Update tip.js

--- tip.js
+++ tip.js
@@ -1,1 +1,15 @@
-
+document.getElementById("calculate-button").addEventListener("click", function() {
+    const billAmount = parseFloat(document.getElementById("bill").value);
+    const serviceQuality = parseFloat(document.getElementById("service-quality").value);
+    
+    if (isNaN(billAmount) || isNaN(serviceQuality)) {
+      alert("Please enter a valid bill amount and service quality.");
+      return;
+    }
+  
+    const tipAmount = billAmount * serviceQuality;
+    const totalAmount = billAmount + tipAmount;
+    
+    document.getElementById("result").innerText = `Tip Amount: $${tipAmount.toFixed(2)} \nTotal Amount: $${totalAmount.toFixed(2)}`;
+  });
+   
